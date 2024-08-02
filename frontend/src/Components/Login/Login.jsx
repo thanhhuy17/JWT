0bbc1e385b9432a7d1bc6781a2c9d1ede2154942
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-08-02 22:08:09 +0700

    update : 1. Login setup input

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,20 +1,37 @@
+import { useState } from "react";
 import "./login.css";
 import { Link } from "react-router-dom";
 const Login = () => {
-    return ( 
-        <section className="login-container">
-            <div className="login-title"> Log in</div>
-            <form>
-                <label>USERNAME</label>
-                <input type="text" placeholder="Enter your username" />
-                <label>PASSWORD</label>
-                <input type="password" placeholder="Enter your password" />
-                <button type="submit"> Continue </button>
-            </form>
-            <div className="login-register"> Don't have an account yet? </div>
-            <Link className="login-register-link" to="/register">Register one for free </Link>
-        </section>
-     );
-}
- 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin=(e)=>{
+    e.preventDefault()
+  }
+  return (
+    <section className="login-container">
+      <div className="login-title"> Log in</div>
+      <form onSubmit={handleLogin}> 
+        <label>USERNAME</label>
+        <input
+          type="text"
+          placeholder="Enter your username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>PASSWORD</label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit"> Continue </button>
+      </form>
+      <div className="login-register"> Don't have an account yet? </div>
+      <Link className="login-register-link" to="/register">
+        Register one for free{" "}
+      </Link>
+    </section>
+  );
+};
+
 export default Login;
--- frontend/src/Components/Register/Register.jsx
+++ frontend/src/Components/Register/Register.jsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import "./register.css";
 const Register = () => {
-    return ( 
-        <section className="register-container">
-              <div className="register-title"> Sign up </div>
-            <form>
-                <label>EMAIL</label>
-                <input type="text" placeholder="Enter your email" />
-                <label>USERNAME</label>
-                <input type="text" placeholder="Enter your username" />
-                <label>PASSWORD</label>
-                <input type="password" placeholder="Enter your password" />
-                <button type="submit"> Create account </button>
-            </form>
-        </section>
-        
-     );
-}
- 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  return (
+    <section className="register-container">
+      <div className="register-title"> Sign up </div>
+      <form>
+        <label>EMAIL</label>
+        <input
+          type="text"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>USERNAME</label>
+        <input
+          type="text"
+          placeholder="Enter your username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>PASSWORD</label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit"> Create account </button>
+      </form>
+    </section>
+  );
+};
+
 export default Register;
